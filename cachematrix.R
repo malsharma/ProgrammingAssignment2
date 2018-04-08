bebd89aa596160865310a02ebab2236f3766c43c
Malvin Sharma <msharma.is@gmail.com>
2018-04-08 22:09:41 +1000

    Commit RProg Assign3 2209

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## THe function below creates a special "matrix" that can cache its inverse
 
+makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inverse) m <<- inverse
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The function below computes the inverse of the special "matrix" created by
+## prior function (makeCacheMatrix). If an inverse already exists, then this is
+## returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+      ## Return a matrix that is the inverse of 'x'
+      
+      m <- x$getinverse()
+      
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      
+      m
 }
